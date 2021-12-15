92bec327fbd880532a452631d60e0360227ab0ee
isaqueveras <isaque.verasccb@gmail.com>
2021-12-15 17:48:55 -0300

    Initializing test on domain of crm/product

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Servers microservices backend
 
 ### Implements microservice of [product](https://github.com/isaqueveras/products-microservice)
-> Product microservice built in golang and gRPC
+> Product microservice build in golang and gRPC
 
 ### Constants of system
 ```go
--- interfaces/crm/product/handler_test.go
+++ interfaces/crm/product/handler_test.go
@@ -1,1 +1,46 @@
 package product
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/isaqueveras/servers-microservices-backend/services"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProduct(t *testing.T) {
+	router := gin.Default()
+
+	if err := services.InitializeConnections(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Initializing routes to crm/product
+	Router(router.Group("v1/crm/products"))
+	RouterWithID(router.Group("/v1/crm/product"))
+
+	// Test of route to get all products
+	t.Run("TestGetAllProducts", func(t *testing.T) {
+		w := httptest.NewRecorder()
+
+		req, err := http.NewRequest("GET", "/v1/crm/products", nil)
+		router.ServeHTTP(w, req)
+
+		assert.Nil(t, err)
+		assert.Equal(t, 200, w.Code)
+	})
+
+	// Test of route to get details of product
+	t.Run("TestGetDetailsProduct", func(t *testing.T) {
+		w := httptest.NewRecorder()
+
+		req, err := http.NewRequest("GET", "/v1/crm/product/16", nil)
+		router.ServeHTTP(w, req)
+
+		assert.Nil(t, err)
+		assert.Equal(t, 200, w.Code)
+	})
+}
